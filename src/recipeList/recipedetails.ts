c6f359ed8102beb4254fd9c118008a3a747bfb18
Tamas Kapitany <tamaskapitany@pm.me>
2026-07-02 21:25:28 +0200

    feat: refactor recipe list and details components for improved structure and functionality

---
+++ src/recipeList/recipedetails.ts
@@ -0,0 +1,32 @@
+import { Component, Input, signal, computed } from '@angular/core';
+import { RecipeModel } from '../app/models';
+import { IngredientModel } from '../app/models';
+
+@Component({
+  selector: 'recipe-details',
+  templateUrl: './recipedetails.html',
+  standalone: true,
+})
+export class RecipeDetails {
+
+    @Input() recipe!: RecipeModel;
+      protected readonly servings = signal(1);
+
+  protected readonly ingredients = computed(() => {
+    return this.recipe.ingredients.map(ingredient => ({
+      ...ingredient,
+      quantity: ingredient.quantity * this.servings(),
+    }));
+  });
+
+    protected increase(): void {
+        this.servings.update(s => s + 1);
+        console.log(`Servings increased to: ${this.servings()}`);
+    }
+
+    protected decrease(): void {
+        this.servings.update(s => Math.max(1, s - 1));
+        console.log(`Servings decreased to: ${this.servings()}`);
+    }
+}
+    
--- src/recipeList/recipelist.ts
+++ src/recipeList/recipelist.ts
@@ -1,10 +1,13 @@
 import { Component, signal, computed} from '@angular/core';
 import { recipes } from '../app/mock-recipes';
 import { RecipeModel } from '../app/models';
+import { RecipeDetails } from "./recipedetails";
 
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipelist.html',
+  standalone: true,
+  imports: [RecipeDetails],
 })
 export class RecipeList {
   constructor() {
@@ -12,29 +15,13 @@ export class RecipeList {
   }
 
   protected readonly selectedRecipe = signal<RecipeModel>(recipes[0]);
-  protected readonly servings = signal(1);
-
-  protected readonly ingredients = computed(() => {
-    return this.selectedRecipe().ingredients.map(ingredient => ({
-      ...ingredient,
-      quantity: ingredient.quantity * this.servings(),
-    }));
-  });
 
   protected choose(recipeId: string): void {
     this.selectedRecipe.set(recipes.find(r => r.id === recipeId) || recipes[0]);
     console.log(`You chose: ${this.selectedRecipe()?.name}`);
   }
 
-  protected increase(): void {
-    this.servings.update(s => s + 1);
-    console.log(`Servings increased to: ${this.servings()}`);
-  }
 
-  protected decrease(): void {
-    this.servings.update(s => Math.max(1, s - 1));
-    console.log(`Servings decreased to: ${this.servings()}`);
-  }
 
 
 }
