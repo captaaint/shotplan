b0e1705263948e5d4927b212709d90c171e46751
Tamas Kapitany <tamaskapitany@pm.me>
2026-07-04 14:38:34 +0200

    feat: add NestJS devtools integration and initialize main application module

---
+++ server/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+
+@Module({
+  imports: [DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+]
+})
+export class AppModule {}
---
+++ server/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  const port = Number(process.env['PORT'] ?? 3000);
+
+  await app.listen(port);
+
+  console.log(`ShotPlan API is running on http://localhost:${port}`);
+}
+
+void bootstrap();
