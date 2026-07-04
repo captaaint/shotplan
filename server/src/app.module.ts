46b2097304e59ac70594f357d7a0f902e0df5c85
Tamas Kapitany <tamaskapitany@pm.me>
2026-07-04 15:59:28 +0200

    feat: remove @nestjs/devtools-integration from dependencies and app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 import { AppController } from './app.controller';
 import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
-  imports: [
-    DevtoolsModule.register({
-      http: process.env['NEST_DEVTOOLS'] === 'true',
-    }),
-    SessionsModule,
-  ],
+  imports: [SessionsModule],
   controllers: [AppController],
 })
 export class AppModule {}
